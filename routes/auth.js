cb83f64c2b1b37b356fab1e6406256bac427dff4
Ousmane Yade <oyade3@gmail.com>
2019-08-05 12:57:28 +0200

    test2

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,6 @@ const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const bcryptSalt = 10
 const Amadeus = require('amadeus')
-console.log(process.env)
 const amadeus = new Amadeus({
   clientId: process.env.AMADEUS_CLIENT_ID,
   clientSecret: process.env.AMADEUS_CLIENT_SECRET
