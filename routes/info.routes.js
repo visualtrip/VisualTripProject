da190ab531f6b6bfbea92df167267cec34773f09
Ousmane Yade <oyade3@gmail.com>
2019-08-07 14:21:54 +0200

    db

--- routes/info.routes.js
+++ routes/info.routes.js
@@ -6,22 +6,22 @@ const Places = require('../models/Places.model')
 const Fligths = require('../models/Flights.model')
 
 
-router.get('/:id', (req,res,next) =>{
+// router.get('/:id', (req,res,next) =>{
 
-  const placeId = req.params.id
-  Places.find({_id : placeId})
-  .then(thePlace => {
-    //console.log(req.user) passport almacena el usuario logeado en el req.user
-    
-    .then(flyInfo =>{
-      console.log(flyInfo.data[0].origin)
-      //console.log(res.body)
-      //res.render('flights-info', flyInfo.data)
-    })
-    .catch(err => console.log('error', console.log(err)))
-      
-  })
-  .catch(err => console.log('error', console.log(err)))
-})
+// //   const placeId = req.params.id
+//   Places.find({_id : placeId})
+//   .then(thePlace => {
+//     //console.log(req.user) passport almacena el usuario logeado en el req.user
+
+//     .then(flyInfo =>{
+//       console.log(flyInfo.data[0].origin)
+//       //console.log(res.body)
+//       //res.render('flights-info', flyInfo.data)
+//     })
+//     .catch(err => console.log('error', console.log(err)))
+
+//   })
+//   .catch(err => console.log('error', console.log(err)))
+// })
 
 module.exports = router
