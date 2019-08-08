4deb99852fffc3128d89221f2d97a47007abe4c3
ebgester <ebg.ester@gmail.com>
2019-08-08 13:09:37 +0200

    deploy heroku

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const MongoStore = require('connect-mongo')(session)
 const flash = require('connect-flash')
 
 mongoose
-  .connect('mongodb://localhost/VisualTrip', { useNewUrlParser: true })
+  .connect(process.env.DBURL, { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
